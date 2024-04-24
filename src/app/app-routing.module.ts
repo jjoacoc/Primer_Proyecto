50f7d715e28a49a36c7268aeb2eb3532cbcfb33c
jjoacoc <jjoaco.cofre@gmail.com>
2024-04-24 10:06:02 -0300

    navbar y cards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { ImagenesComponent } from './imagenes/imagenes.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'inicio', component: InicioComponent },
+{ path: 'galeria', component: ImagenesComponent },
+{ path: '', component:InicioComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
