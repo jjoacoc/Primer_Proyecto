560827357c5a2b6c1ba0439965d524081607c1bf
jjoacoc <jjoaco.cofre@gmail.com>
2024-04-24 11:14:20 -0300

    creacion de primeros modulos y configuracion de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './modules/inicio/pages/inicio/inicio.component';
 
-const routes: Routes = [];
+//son las encargadas de tener todas las rutas de la pagina
+const routes: Routes = [
+  //ruta comun -> 1 solo componente
+{
+path:'',
+component:InicioComponent
+},
+//ruta perezosa -> 1 modulo
+//loadChildren: indica una ruta hija
+//()=>import: ruta en donde viene el modulo
+// .then: promesa/ funcion asincronica
+
+{
+path:'',
+loadChildren:()=>import('./modules/inicio/inicio.module').then(m=>m.InicioModule)
+},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
