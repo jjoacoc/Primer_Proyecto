b16f92a0ec5597078d9b36ef5bce19f2835d815f
jjoacoc <jjoaco.cofre@gmail.com>
2024-05-29 10:03:37 -0300

    solucionamos el error en el registro y creamos una nueva interfaz llamada usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './modules/shared/shared.module';
-import { InicioSesionComponent } from './modules/auntentificacion/pages/inicio-sesion/inicio-sesion.component';
+import { InicioSesionComponent } from './modules/autentificacion/pages/inicio-sesion/inicio-sesion.component';
 
 @NgModule({
   declarations: [
---
+++ src/app/models/usuario.ts
@@ -0,0 +1,7 @@
+export interface Usuario {
+    uid: string | any;  //attribs tiipo any = recibe valores indefinidos
+    nombre: string;
+    apellido: string;
+    email: string;
+    password: string ;
+}
