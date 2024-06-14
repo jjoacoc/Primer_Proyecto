181721e8a892432680f82da2ce5ae4fc29bd327e
cvpedro66 <cuevasp931@gmail.com>
2024-06-14 11:01:29 -0300

    configuracion de inicioSesion con servicio Auth y servicio Firestore para acceder con usuarios existentes en la BD

--- src/app/modules/autentificacion/pages/inicio-sesion/inicio-sesion.component.ts
+++ src/app/modules/autentificacion/pages/inicio-sesion/inicio-sesion.component.ts
@@ -1,21 +1,27 @@
 import { Component } from '@angular/core';
 import { Usuario } from 'src/app/models/usuario';
+import { AuthService } from '../../services/auth.service';
+import { FirestoreService } from 'src/app/modules/shared/services/firestore.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio-sesion',
   templateUrl: './inicio-sesion.component.html',
-  styleUrls: ['./inicio-sesion.component.css']
+  styleUrls: ['./inicio-sesion.component.css'],
 })
 export class InicioSesionComponent {
-
   //este "hide" es para el input de contraseña
   hide = true;
+  //###########################################LOCAL####################################################################
+  /*
+  declaro la variable credenciales con el arreglo Usuario
 
-  //declaro la variable credenciales con el arreglo Usuario
   public credenciales: Usuario[];
 
-  //creo un constructor que va a entrar a el arreglo de la variable credenciales y va a almacenar 2 objetos de ejemplos de usuario
-  constructor() {
+  creo un constructor que va a entrar a el arreglo de la variable credenciales y va a almacenar 2 objetos de ejemplos de usuario
+  constructor(
+    
+  ) {
     this.credenciales = [
       {
         nombre: 'pepe',
@@ -34,11 +40,20 @@ export class InicioSesionComponent {
         password: '1234562',
       }
     ]
-    // console.log(this.credenciales)
+  console.log(this.credenciales)
 
   }
 
-  //creo un objeto usuarios que va a recibir toda la informacion que envie con el boton de el html en este caso solo email y password
+  creo un objeto usuarios que va a recibir toda la informacion que envie con el boton de el html en este caso solo email y password
+  */
+  //##############################################FIN LOCAL###############################################################
+
+  constructor(
+    public servicioAuth: AuthService,
+    public servicioFirestore: FirestoreService,
+    public servicioRutas: Router
+  ) {}
+
   usuarios: Usuario = {
     uid: '',
     nombre: '',
@@ -46,12 +61,17 @@ export class InicioSesionComponent {
     email: '',
     rol: '',
     password: '',
-  }
+  };
 
-  //FUNCION PARA EL REGISTRO
-  //funcion flecha que declara la variable comprobar y se mete en el arreglo credenciales en el cual crea una variable "temporal" llamada busca que va a decir si busca en el objeto usuario el email y esto es = a el email de el arreglo credenciales entonces tambien va a buscar si la contraseña es la misma de el arreglo y si es la misma va a tirar en la consola el objeto completo de el arreglo con todos sus datos
+  /*
+  FUNCION PARA EL REGISTRO
+  funcion flecha que declara la variable comprobar y se mete en el arreglo credenciales en el cual crea una variable "temporal" llamada busca que va a decir si busca en el objeto usuario el email y esto es = a el email de el arreglo credenciales entonces tambien va a buscar si la contraseña es la misma de el arreglo y si es la misma va a tirar en la consola el objeto completo de el arreglo con todos sus datos
+  */
 
-  comprobacion() {
+  //############################################INICIO DE SESION########################################################
+
+  async comprobacion() {
+    /*
     const comprobar = this.credenciales.find((busca) => busca.email === this.usuarios.email && this.usuarios.password === busca.password)
 
     if (comprobar) {
@@ -67,6 +87,31 @@ export class InicioSesionComponent {
       email: this.usuarios.email = '',
       password: this.usuarios.password = '',
     }
+        */
+
+    const credenciales = {
+      email: this.usuarios.email,
+      password: this.usuarios.password,
+    };
+
+    const res = await this.servicioAuth
+      .iniciarSesion(credenciales.email, credenciales.password)
+      .then((res) => {
+        alert('Se ha logeado con exito');
+        this.servicioRutas.navigate(['/inicio']);
+      })
+      .catch((err) => {
+        alert('Hubo un problema al iniciar sesion' + err);
+        this.limpiarInputs();
+      });
   }
 
+  limpiarInputs() {
+    const inputs = {
+      email: (this.usuarios.email = ''),
+      password: (this.usuarios.password = ''),
+    };
+  }
+
+  //############################################FIN INICIO DE SESION#####################################################
 }
