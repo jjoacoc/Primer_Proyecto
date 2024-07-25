30dbf7930149fc0e44fb73641c00233dbe2410d9
jjoacoc <jjoaco.cofre@gmail.com>
2024-07-25 09:43:22 -0300

    Trabajamos con directiva *ngIf para ocultar y mostrar botones de autentificacion en el navegador

--- src/app/modules/shared/components/navbar/navbar.component.ts
+++ src/app/modules/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/modules/autentificacion/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+
+  logueado = true; // Variable booleana para el boton de registro e Inicio de sesion
+  deslogueado = false; // Variable booleana para el boton de Cerrar sesion
+
+  constructor(
+    public servicioAuth: AuthService,
+    public servicioRutas: Router
+  ) { }
+
+  // Cambia los valores del logeado y deslogueado para ocultar los primeros y mostrar el ultimo
+  iniciar(){
+    this.logueado = false;
+    this.deslogueado = true;
+  }
+
+  cerrarSesion(){
+    this.deslogueado = false;
+    // Va a eliminar el "token" actual del usuario
+    // token: 
+    this.servicioAuth.cerrarSesion();
+
+    this.servicioRutas.navigate(['/']); //redirigimos a la raiz de la pagina
+    this.logueado = true;
+  }
+
+  
 
 }
